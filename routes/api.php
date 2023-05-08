995e086d4ec0e4232fe6132f2cc2b46105503a3b
htetoozin <htetoo.zin09@gmail.com>
2023-05-09 02:14:08 +0630

    [modify] fixed route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\RegisteredUserController;
 use App\Http\Controllers\Api\V1\AuthenticatedController;
 
 /*
